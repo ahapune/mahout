fb08fa9c9eb888f3472cac0c7a06afdff4283537
shubhanan <shubhanan.deshpande@talentica.com>
2014-07-01 18:17:19 +0530

    Publishing results for manual verification of recommendations. Configured to talk to QA DB by default.

--- AhalifeRecommender/src/main/java/com/ahalife/AhalifeRecommender.java
+++ AhalifeRecommender/src/main/java/com/ahalife/AhalifeRecommender.java
@@ -1,6 +1,11 @@
 package com.ahalife;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.eval.DataModelBuilder;
@@ -13,10 +18,13 @@ import org.apache.mahout.cf.taste.impl.model.GenericBooleanPrefDataModel;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.impl.recommender.GenericBooleanPrefItemBasedRecommender;
 import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.model.Preference;
 import org.apache.mahout.cf.taste.model.PreferenceArray;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.apache.mahout.cf.taste.recommender.Recommender;
 import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
 import org.apache.mahout.common.RandomUtils;
+
 
 public class AhalifeRecommender {
 
@@ -41,10 +49,70 @@ public class AhalifeRecommender {
         return new GenericBooleanPrefDataModel(GenericBooleanPrefDataModel.toDataMap(trainingData));
       }
     };
+    
+    int userId = 177;
+    PreferenceArray p = model.getPreferencesFromUser(userId);
+    Iterator<Preference> pItr = p.iterator();
+
+    Map<Long, Float> ratingMap = new HashMap<Long, Float>();
+    Preference pref;
+    while (pItr.hasNext()) {
+    	pref = pItr.next();
+    	ratingMap.put(pref.getItemID(), pref.getValue());
+    }
+    List<Long> ids = new ArrayList<Long>();
+    ids.addAll(ratingMap.keySet());
+    List<Map<String, Object>> productInfo = JDBCDao.getProductInfo(ids);
+        
+    System.out.println("The User ID " + userId + " has bought following items, following times:");
+    System.out.println( "ID \t\t\t\t NAME \t\t\t\t #BUYS \t\t\t\t\t URL");
+    System.out.println( "== \t\t\t\t ==== \t\t\t\t ===== \t\t\t\t\t ===");
+    
+    Iterator<Map<String, Object>> lItr = productInfo.iterator();
+    Long productId;
+    String url, name;
+    Map<String, Object> map;
+    while (lItr.hasNext()) {
+    	map = lItr.next();
+    	productId = ((Long) map.get("PRODUCT_ID"));
+    	url = (String) map.get("URL");
+    	name = ((String) map.get("NAME")).trim();
+    	System.out.println( productId + " \t\t\t " + name + " \t\t\t " + ratingMap.get(productId) + " \t\t\t " + "www.ahalife.com/"+url);
+    }
+    
     IRStatistics stats =
         evaluator.evaluate(recommenderBuilder, modelBuilder, model, null, 1,
             GenericRecommenderIRStatsEvaluator.CHOOSE_THRESHOLD, 0.8);
-    System.out.println( recommenderBuilder.buildRecommender(model).recommend(177, 3));
+    List<RecommendedItem> reco =  recommenderBuilder.buildRecommender(model).recommend(userId, 5);
+    
+    System.out.println("\n\nThe Recommended Products for this user were:");
+    Iterator<RecommendedItem> recoItr = reco.iterator();
+    RecommendedItem r;
+    Map<Long, Float> recoMap = new HashMap<Long, Float>();
+    
+    while(recoItr.hasNext()) {
+    	r = recoItr.next();
+    	recoMap.put(r.getItemID(), r.getValue());
+    }
+    List<Long> recommendedIds = new ArrayList<Long>();
+    recommendedIds.addAll(recoMap.keySet());
+    productInfo = JDBCDao.getProductInfo(recommendedIds);
+    System.out.println( "ID \t\t\t\t VALUE \t\t\t NAME \t\t\t\t\t URL");
+    System.out.println( "== \t\t\t\t ===== \t\t\t ==== \t\t\t\t\t ===");
+    lItr = productInfo.iterator();
+    Float value;
+    while (lItr.hasNext()) {
+    	map = lItr.next();
+    	productId = ((Long) map.get("PRODUCT_ID"));
+    	url = (String) map.get("URL");
+    	name = ((String) map.get("NAME")).trim();
+    	value = recoMap.get(productId);
+    	System.out.println( productId + " \t\t\t " + value + " \t\t " + name + " \t\t\t " + "www.ahalife.com/products/"+productId);
+    	
+    }
+    
+    
+    System.out.println("\n\nThe Precision and Recall in the data-set was:");
     System.out.println(stats.getPrecision());
     System.out.println(stats.getRecall());
   }
--- AhalifeRecommender/src/main/java/com/ahalife/JDBCDao.java
+++ AhalifeRecommender/src/main/java/com/ahalife/JDBCDao.java
@@ -20,7 +20,13 @@ public class JDBCDao {
   private static NamedParameterJdbcTemplate namedJdbcTemplate;
   static DriverManagerDataSource dataSource;
   private static String GET_PRODUCTS_CATEGORIES =
-      "select bcpr.CATEGORY_ID,bcpr.PRODUCT_ID from blc_product bp join blc_category_product_xref bcpr using (PRODUCT_ID) where bp.PRODUCT_ID in (:ids)";
+      "select bcpr.CATEGORY_ID,bcpr.PRODUCT_ID from blc_product bp " +
+      "join blc_category_product_xref bcpr using (PRODUCT_ID) " +
+      "where bp.PRODUCT_ID in (:ids)";
+  private static String GET_PRODUCT_INFO = "select bp.`product_id`, bs.`name`, bp.`url` from blc_product bp " +
+  		"join blc_sku bs on bp.`PRODUCT_ID`=bs.`DEFAULT_PRODUCT_ID` " +
+  		"where bp.`PRODUCT_ID`in (:ids)";
+
   private static Multimap<Long, Long> categoryList;
   static List<Long> productIds ;
   public JDBCDao() {
@@ -41,9 +47,9 @@ public class JDBCDao {
       dataSource = new DriverManagerDataSource();
       dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 
-      dataSource.setUsername("root");
-      dataSource.setUrl("jdbc:mysql://localhost:3306/broadleaf?autoReconnect=true");
-      dataSource.setPassword("root");
+      dataSource.setUsername("apivalve");
+      dataSource.setUrl("jdbc:mysql://sql.qa.ahalife.com:3306/broadleaf_mobile?autoReconnect=true");
+      dataSource.setPassword("iexda1dXgAMa");
     }
     return dataSource;
   }
@@ -67,6 +73,18 @@ public class JDBCDao {
     return categoryList;
   }
 
+  public static List<Map<String, Object>> getProductInfo (List<Long> ids) {
+	  
+	  //get IDs
+	  Map<String, List<Long>> productInfoParam = new HashMap<String, List<Long>>();
+	  
+	  	productInfoParam.put("ids", ids);
+	  List<Map<String, Object>> productInfo = getNamedParameterJdbcTemplate().queryForList(GET_PRODUCT_INFO, productInfoParam);
+	  
+	  
+	  return productInfo;
+  }
+  
   public static List<Long> getProductCategories(Long id) {
    
     return (List<Long>) getCategoriesList().get(id);
